acd56d1a57c5e3d0c887caaaf403430e716e4309
alexmarshak <55095785+alexmarshak@users.noreply.github.com>
2019-10-08 09:16:40 -0400

    button pass

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-document.getElementById("demo").innerHTML = "My First JavaScript";
+function hello(){
+  // var response=prompt("What is your name?");
+  // alert("hello, "+response);
+  // document.getElementById("demo").innerHTML = "Completed";
+  console.log("java")
+}
+function password(){
+  let pwd = "123abc";
+  let user = prompt("Enter the password. You have 5 guesses");
+  for ( let i=0; i<4; i++){
+    user = prompt("Wrong. Enter the password:");
+  }
+}
+
+
 
